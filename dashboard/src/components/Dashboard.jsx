133d423c1ac2eddf11ed3efa73a2d1d3ff22bce6
shubham Pati <patilshubham1261@gmail.com>
2026-01-19 17:28:39 +0530

    Dashboard Design

---
+++ dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Summary from "./Summary";
+import Orders from "./Orders";
+import Holdings from "./Holdings";
+import Positions from "./Positions";
+import Funds from "./Funds";
+import WatchList from "./WatchList";
+
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="dashboard-container">
+        <WatchList />
+        <div className="content"></div>
+         <Routes>
+          <Route path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
---
+++ dashboard/src/components/Orders.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Orders() {
+  return (
+    <>
+      <div className="orders">
+        <div className="no-orders">
+          <p>You haven't placed any orders today</p>
+
+          <Link to={"/"} className="btn">
+            Get started
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
