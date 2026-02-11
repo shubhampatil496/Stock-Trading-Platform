db75d82c9caa5ab2aaa3b2ab8d47b6f99825299c
shubham Pati <patilshubham1261@gmail.com>
2026-02-11 18:30:04 +0530

    add buy functionality

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -6,23 +6,25 @@ import Positions from "./Positions";
 import Funds from "./Funds";
 import Apps from "./Apps";
 import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
 
 
 export default function Dashboard() {
   return (
-    <>
-      <div className="dashboard-container">
+    <div className="dashboard-container">
+      <GeneralContextProvider>
         <WatchList />
-        <div className="content"></div>
-         <Routes>
-          <Route path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
-        </Routes>
-      </div>
-    </>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Summary />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/holdings" element={<Holdings />} />
+            <Route path="/positions" element={<Positions />} />
+            <Route path="/funds" element={<Funds />} />
+            <Route path="/apps" element={<Apps />} />
+          </Routes>
+        </div>
+      </GeneralContextProvider>
+    </div>
   );
 }
